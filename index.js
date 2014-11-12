b8be6e098b334f824d0e3cc222e9a89c56923be4
Jake Bixby <trabus@gmail.com>
2014-11-12 00:30:38 -0800

    initial build and podify command

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
+function Addon(){
+  this.name = 'ember-cli-podify';
+}
 
-module.exports = {
-  name: 'ember-cli-podify'
-};
+Addon.prototype.includedCommands = function(){
+  return {
+    'Podify': {
+      name: 'podify',
+      aliases: ['pod','pf']
+    },
+    'Depodify': {
+      name: 'depodify',
+      aliases: ['depod','dp']
+    }
+  }
+}
+
+module.exports = Addon;
